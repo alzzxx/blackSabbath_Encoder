681c1557a61a2d45e04f83e3663df457c4883287
Alex Eduardo Rondon Reggio <aerr.3907@gmail.com>
2021-04-27 16:29:05 +0200

    1

---
+++ build/core/itoa.c.d
@@ -0,0 +1,4 @@
+c:\Users\a.rondon\Documents\GitHub\blackSabbath_Encoder\build\core\itoa.c.o: \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino\itoa.c \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino/api/itoa.h \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino/api/deprecated-avr-comp/avr/dtostrf.c.impl
