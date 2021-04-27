ee05366efc928f64ce05dbe76839859a76cb76b3
Alex Eduardo Rondon Reggio <aerr.3907@gmail.com>
2021-04-27 16:31:14 +0200

    1

---
+++ build/core/api/Common.cpp.d
@@ -0,0 +1,3 @@
+c:\Users\a.rondon\Documents\GitHub\blackSabbath_Encoder\build\core\api\Common.cpp.o: \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino\api\Common.cpp \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino\api\Common.h
---
+++ build/core/api/Print.cpp.d
@@ -0,0 +1,6 @@
+c:\Users\a.rondon\Documents\GitHub\blackSabbath_Encoder\build\core\api\Print.cpp.o: \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino\api\Print.cpp \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino\api\Print.h \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino\api\String.h \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino\api\deprecated-avr-comp/avr/pgmspace.h \
+ C:\Users\a.rondon\AppData\Local\Arduino15\packages\arduino\hardware\mbed\1.3.2\cores\arduino\api\Printable.h
