f47458c084a1c05b95f257db864366a040fc6ee1
Alex Eduardo Rondon Reggio <aerr.3907@gmail.com>
2021-04-27 16:26:54 +0200

    added missing properties files

---
+++ build/libraries/Adafruit_GFX_Library/glcdfont.c.d
@@ -0,0 +1,2 @@
+c:\Users\a.rondon\Documents\GitHub\blackSabbath_Encoder\build\libraries\Adafruit_GFX_Library\glcdfont.c.o: \
+ C:\Users\a.rondon\Documents\Arduino\libraries\Adafruit_GFX_Library\glcdfont.c
